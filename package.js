f6c6f6fd374e04538fe512b6309a07a53e5101bd
Gabriel Rubens <grsabreu@gmail.com>
2016-05-03 13:46:49 -0300

    Update package

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'astrocoders:one-signal',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Simple OneSignal integration for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Astrocoders/meteor-one-signal',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
